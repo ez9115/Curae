ffda288d52246983d2154dc73c56a6a5dbbf04e1
Josh Killingsworth <joshkillingsworth@Joshs-MacBook-Pro.local>
2014-06-05 20:55:23 -0400

    Checkin of further code in Main and AverageSentenceLength that reads file and reads word length between delimeters

--- 6300Sum14Project1/src/gatech/cs6300/project1/Main.java
+++ 6300Sum14Project1/src/gatech/cs6300/project1/Main.java
@@ -1,9 +1,29 @@
 package gatech.cs6300.project1;
 
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int len  = 0;
+	static String fileName="";
+	static String delimeter;
+	static File inputFile;
+	
+	public static void main(String[] args) throws IOException {
+		try {
+			fileName = args[0];
+			delimeter= args[1];
+			System.out.println("delimeter:"+ args[1]);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("File="+fileName);
+		setFile(new File(fileName));
+		setSentenceDelimiters(args[1]);
+		computeAverageSentenceLength();
 
 	}
 
